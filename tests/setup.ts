9cec1090d81bb361a578c6e64904685e09c81e97
Craig R. Hughes <craig.git@rungie.com>
2025-12-30 22:26:20 -0500

    Silence output during tests to prevent stryker from blowing up

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,6 @@
 // Test setup and configuration
+//
+// Silence all log messages while in test to avoid blowing up stryker
+import { forEach } from 'lodash';
+import { logger } from '@hughescr/logger';
+forEach(logger.transports, t => t.silent = true);
