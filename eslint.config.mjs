c6b96e3008af9f2e714568936f9115ddec777c4f
Craig R. Hughes <craig.git@rungie.com>
2026-01-09 12:19:28 -0800

    Exclude reports/ from eslint coverage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import config from '@hughescr/eslint-config-default';
 export default [
     ...config,
     {
-        ignores: ['scratch/', 'dist/', 'node_modules/', '.stryker-tmp/', '.sst/', 'sst.config.ts', 'sst/**/*', 'sst-env.d.ts', 'plugins/**']
+        ignores: ['scratch/', 'dist/', 'node_modules/', '.stryker-tmp/', 'reports/', '.sst/', 'sst.config.ts', 'sst/**/*', 'sst-env.d.ts', 'plugins/**']
     },
     {
         rules: {
