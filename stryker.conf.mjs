9620b2f722df5e5b62d2f1a49286a71f6ad9e9a3
Craig R. Hughes <craig.git@rungie.com>
2026-02-04 14:51:01 -0800

    Add tools to stryker sandbox because we do have tool tests!

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -12,7 +12,7 @@ export default {
     coverageAnalysis: 'perTest',
     disableBail:      true, // Do not stop with first failing test, so we can get complete map of mutant:killer-tests
     mutate:           ['src/**/*.ts', '!src/index.ts'], // Do not mutate the entry point
-    ignorePatterns:   ['**', '!src/**/*.ts', '!tests/**/*.ts', '!bunfig.toml', '!tsconfig.json', '!*.ts', '!*.mjs', '!sst/**/*.ts'], // Only include source and test files in the mutation testing process
+    ignorePatterns:   ['**', '!src/**/*.ts', '!tests/**/*.ts', '!tests/**/*.json', '!tools/**/*.ts', '!bunfig.toml', '!tsconfig.json', '!*.ts', '!*.mjs', '!sst/**/*.ts'], // Only include source and test files in the mutation testing process
     thresholds:       { high: 100, low: 100, 'break': 100 },
     concurrency:      isCI ? 4 : 24,
     tempDirName:      '.stryker-tmp',
